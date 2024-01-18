50ae034798343e1e2ef3c411542bcae3b5606925
Maciej Labedzki <maciej.labedzki@codewave.pl>
2024-01-18 14:38:05 +0100

    add

--- 003/12/index.js
+++ 003/12/index.js
@@ -3,20 +3,22 @@ FlotiqPlugins.add(
       id: 'PluginsML.layout-sidebar-menu-item',
       displayName: `Add menu item`,
       description: "Lorem ipsum",
-      version: `1.0.11`, 
+      version: `1.0.12`, 
     },
     function (handler) {
       handler.on(
         'flotiq.layout.sidebar-menu-item::add',
         ({ permissions, isRoleAdmin, user, isAdmin, icons }) => {
            
-          console.log('Valide input',permissions, isRoleAdmin, user, isAdmin ); 
-           
-          console.log('Valide icons',permissions, isRoleAdmin, user, isAdmin, icons);   
+          console.log('Layout Param: permissions',permissions ); 
+          console.log('Layout Param: isRoleAdmin',isRoleAdmin ); 
+          console.log('Layout Param: user',user ); 
+          console.log('Layout Param: isAdmin',isAdmin );
+          console.log('Layout Param: icons', icons);   
             
             return [{
                 key: 'new-menu-item-1',
-                icon "♂",
+                icon: "♂",
                 title: "New Link",
                 link: '/',
                 target: '',
@@ -24,7 +26,7 @@ FlotiqPlugins.add(
                 className: '', 
               },{
                 key: 'new-menu-item-2',
-                icon:   "♪",
+                icon: "♪",
                 title: "Another Link",
                 link: '/',
                 target: '',
