19f88d73bbe2815b70bfa458368eaca7fdc6547e
Maciej Labedzki <maciej.labedzki@codewave.pl>
2024-01-18 09:18:08 +0100

    add

---
+++ 002/index.js
@@ -0,0 +1,31 @@
+FlotiqPlugins.add(
+    {
+      id: 'PluginsML.title-hide-group',
+      displayName: `Hide title and discover on hover`,
+      version: `1.0.0`, 
+    },
+    function (handler) {
+      handler.on(
+        'flotiq.grid.cell::render',
+        ({ contentType, accessor, data }) => {
+           
+          if (contentType?.name !== 'blogposts') return null;
+          if (accessor != 'title') return null;  
+
+            // Create Grup Div
+            const divContainer = document.createElement('div');
+            divContainer.className = "group";
+
+            // Create new span element with current title
+            const spanTitle = `<span class="hidden group-hover:block">${data}</span>`;
+
+            // Creater new span element with hide placeholder
+            const spanHidePlaceholderTitle = `<span class="block group-hover:hidden ">🔍 SHOW TITLE ON HOVER</span>`;        
+        
+            divContainer.innerHTML = spanTitle + spanHidePlaceholderTitle;
+        
+            return divContainer;
+        },
+      );
+    },
+  );
