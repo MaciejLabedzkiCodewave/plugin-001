5050afb4400ee00d2d169c43b31d41c85d247da3
Maciej Labedzki <maciej.labedzki@codewave.pl>
2024-01-18 14:34:32 +0100

    add

---
+++ 003/11/index.js
@@ -0,0 +1,37 @@
+FlotiqPlugins.add(
+    {
+      id: 'PluginsML.layout-sidebar-menu-item',
+      displayName: `Add menu item`,
+      description: "Lorem ipsum",
+      version: `1.0.11`, 
+    },
+    function (handler) {
+      handler.on(
+        'flotiq.layout.sidebar-menu-item::add',
+        ({ permissions, isRoleAdmin, user, isAdmin, icons }) => {
+           
+          console.log('Valide input',permissions, isRoleAdmin, user, isAdmin ); 
+           
+          console.log('Valide icons',permissions, isRoleAdmin, user, isAdmin, icons);   
+            
+            return [{
+                key: 'new-menu-item-1',
+                icon: icons.docs || "♂",
+                title: "New Link",
+                link: '/',
+                target: '',
+                rel: '',
+                className: '', 
+              },{
+                key: 'new-menu-item-2',
+                icon:  icons.docs || "♪",
+                title: "Another Link",
+                link: '/',
+                target: '',
+                rel: '',
+                className: '', 
+              }];
+        },
+      );
+    },
+  );
