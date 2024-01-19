79ee4a3ba492574748ee6d55c404c6a58075a800
Maciej Labedzki <maciej.labedzki@codewave.pl>
2024-01-19 09:52:24 +0100

    add

---
+++ 003/21/index.js
@@ -0,0 +1,44 @@
+FlotiqPlugins.add(
+  {
+    id: 'PluginsML.layout-sidebar-menu-item',
+    displayName: `Add menu item`,
+    description: "Lorem ipsum",
+    version: `1.0.21`, 
+  },
+  function (handler) {
+    handler.on(
+      'flotiq.layout.sidebar-menu-item::add',
+      ({ permissions, isRoleAdmin, user, isAdmin, icons }) => {
+
+        const menuItems = [];
+
+        if(isAdmin){
+          menuItems.push(
+            {
+              key: 'sentry-link',
+              iconSrc: "https://raw.githubusercontent.com/MaciejLabedzkiCodewave/plugin-001/main/images/sentry.svg",
+              title: "Sentry",
+              link: 'https://sentry.dev.cdwv.pl/organizations/cdwv/issues/',
+              target: '_blank',
+              rel: 'noreferer',
+              className: undefined, 
+            }
+          )
+        }
+
+        menuItems.push({
+          key: 'flotiq-page-manu-item',
+          iconSrc: "https://raw.githubusercontent.com/MaciejLabedzkiCodewave/plugin-001/main/images/flotiqBadgeLogo.svg",
+          title: 'Flotiq Page',
+          link: 'https://flotiq.com/',
+          target: '_blank',
+          rel: 'noreferer',
+          className: undefined, 
+        })
+
+          
+        return menuItems;
+      },
+    );
+  },
+);
