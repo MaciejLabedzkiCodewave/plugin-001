57c8e578d300b91385eacdf28ae9a6f3a58493b3
Maciej Labedzki <maciej.labedzki@codewave.pl>
2024-02-12 23:05:46 +0100

    add

---
+++ develop-netlify/11/index.js
@@ -0,0 +1,66 @@
+FlotiqPlugins.add(
+    {
+      id: 'PluginsML.deploy-netlify',
+      displayName: `Deploy Netlify`,
+      description: "Integration with Netlify",
+      version: "1.0.11", 
+      repository: "https://raw.githubusercontent.com/MaciejLabedzkiCodewave/plugin-001/main/develop-netlify/1/index.js",
+      permissions: []
+    },
+    function (handler) {
+      handler.on(
+        'flotiq.plugin.library::add',
+        ({excludes}) => { 
+
+
+          // Validate Parsable String
+          function isJsonParsable(str) {
+              try {
+                  JSON.parse(str);
+              } catch (e) {
+                  return false;
+              }
+              return true;
+          }
+
+          // Case: is plugin excluded  
+          if(typeof excludes === "string" && 
+            excludes?.length > 0 &&
+            isJsonParsable(excludes) &&
+            JSON.parse(excludes)?.includes("Netlify Build")) {
+            return null;
+          }
+
+          // Case: Netlify Build Settings
+          return {
+              "type": "netlify-build",
+              "code": "__API_URL__/bundles/netlifybuild/netlify-build/button.html",
+              "name": "Netlify Build",
+              "version": "0.2",
+              "internal": {
+                  "createdAt": "",
+                  "deletedAt": "",
+                  "updatedAt": "",
+                  "contentType": "_plugin"
+              },
+              "settings": [
+                  {
+                      "key": "build_instance_url",
+                      "label": "Build Instance URL",
+                      "value":"",
+                  },
+                  {
+                      "key": "build_webhook_url",
+                      "label": "Build Webhook URL",
+                      "value":"",
+                  }
+              ],
+              "description": "Integration with the Netlify. After adding the plugin, you will be able to build your page on Netlify within Flotiq.",
+              "contentTypeDefinitions": "",
+              "order": "-1",
+              "disabled": false
+            }
+        },
+      );
+    },
+  );
---
+++ develop-netlify/_arch/10/index.js
@@ -0,0 +1,46 @@
+FlotiqPlugins.add(
+    {
+      id: 'PluginsML.deploy-netlify',
+      displayName: `Deploy Netlify`,
+      description: "Integration with Netlify",
+      version: "1.0.10", 
+      repository: "https://raw.githubusercontent.com/MaciejLabedzkiCodewave/plugin-001/main/develop-netlify/1/index.js",
+      permissions: []
+    },
+    function (handler) {
+      handler.on(
+        'flotiq.plugin.library::add',
+        ({ finalCtdOptions }) => {
+
+          return {
+              "type": "netlify-build",
+              "code": "__API_URL__/bundles/netlifybuild/netlify-build/button.html",
+              "name": "Netlify Build",
+              "version": "0.2",
+              "internal": {
+                  "createdAt": "",
+                  "deletedAt": "",
+                  "updatedAt": "",
+                  "contentType": "_plugin"
+              },
+              "settings": [
+                  {
+                      "key": "build_instance_url",
+                      "label": "Build Instance URL",
+                      "value":"",
+                  },
+                  {
+                      "key": "build_webhook_url",
+                      "label": "Build Webhook URL",
+                      "value":"",
+                  }
+              ],
+              "description": "Integration with the Netlify. After adding the plugin, you will be able to build your page on Netlify within Flotiq.",
+              "contentTypeDefinitions": "",
+              "order": "-1",
+              "disabled": false
+            }
+        },
+      );
+    },
+  );
