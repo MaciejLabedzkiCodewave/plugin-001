d76b78f7ea11561af7bd8300a1e539758beb5435
Maciej Labedzki <maciej.labedzki@codewave.pl>
2024-02-12 17:01:00 +0100

    add

---
+++ develop-netlify/2/index.js
@@ -0,0 +1,40 @@
+FlotiqPlugins.add(
+    {
+      id: 'PluginsML.deploy-netlify',
+      displayName: `Deploy Netlify`,
+      description: "add",
+      version: `1.0.0`, 
+    },
+    function (handler) {
+      handler.on(
+        'flotiq.plugin.library::add',
+        ({ finalCtdOptions }) => {
+          
+            return {
+                "type": "netlify-build",
+                "code": "__API_URL__/bundles/netlifybuild/netlify-build/button.html",
+                "name": "Netlify Build",
+                "version": "0.2",
+                "internal": {
+                    "createdAt": "2020-04-20T14:00:40+00:00",
+                    "deletedAt": "",
+                    "updatedAt": "2020-04-21T12:16:21+00:00",
+                    "contentType": "_plugin"
+                },
+                "settings": [
+                    {
+                        "key": "build_instance_url",
+                        "label": "Build Instance URL"
+                    },
+                    {
+                        "key": "build_webhook_url",
+                        "label": "Build Webhook URL"
+                    }
+                ],
+                "description": "Integration with the Netlify. After adding the plugin, you will be able to build your page on Netlify within Flotiq.",
+                "contentTypeDefinitions": finalCtdOptions
+            }
+        },
+      );
+    },
+  );
