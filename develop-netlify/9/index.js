49bb5cca7322b24664eac28cc00298f09c5046fa
Maciej Labedzki <maciej.labedzki@codewave.pl>
2024-02-12 21:04:57 +0100

    add

--- develop-netlify/9/index.js
+++ develop-netlify/9/index.js
@@ -3,7 +3,7 @@ FlotiqPlugins.add(
       id: 'PluginsML.deploy-netlify',
       displayName: `Deploy Netlify`,
       description: "Integration with Netlify",
-      version: "1.0.8", 
+      version: "1.0.9", 
       repository: "https://raw.githubusercontent.com/MaciejLabedzkiCodewave/plugin-001/main/develop-netlify/1/index.js",
       permissions: []
     },
@@ -35,8 +35,8 @@ FlotiqPlugins.add(
               ],
               "description": "Integration with the Netlify. After adding the plugin, you will be able to build your page on Netlify within Flotiq.",
               "contentTypeDefinitions": "",
-              "order": "4",
-              "disabled" : "true"
+              "order": 4,
+              "disabled" : true
             }
         },
       );
