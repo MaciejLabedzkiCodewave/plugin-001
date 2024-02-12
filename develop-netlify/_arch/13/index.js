ba811b5c35cf280856531edfbd0c118e7e504d65
Maciej Labedzki <maciej.labedzki@codewave.pl>
2024-02-12 23:26:43 +0100

    aaa

---
+++ develop-netlify/_arch/13/index.js
@@ -0,0 +1,63 @@
+FlotiqPlugins.add(
+    {
+      id: 'PluginsML.deploy-netlify',
+      displayName: `Deploy Netlify`,
+      description: "Integration with Netlify",
+      version: "1.0.13", 
+      repository: "https://raw.githubusercontent.com/MaciejLabedzkiCodewave/plugin-001/main/develop-netlify/12/index.js",
+      permissions: []
+    },
+    function (handler) {
+      handler.on(
+        'flotiq.plugin.library::add',
+        ({exclude}) => {  
+          // Validate Parsable String
+          function isJsonParsable(str) {
+              try {
+                  JSON.parse(str);
+              } catch (e) {
+                  return false;
+              }
+              return true;
+          }
+
+          // Case: is this plugin excluded  
+          if(exclude?.length > 0 &&
+            isJsonParsable(exclude) &&
+            JSON.parse(exclude)?.includes("Netlify Build")) {
+            return null;
+          }
+
+          // Case: Netlify Build Settings
+          return {
+              "type": "netlify-build",
+              "code": "__API_URL__/bundles/netlifybuild/netlify-build/button.html",
+              "name": "Netlify Build",
+              "version": "0.2",
+              "internal": {
+                  "createdAt": "",
+                  "deletedAt": "",
+                  "updatedAt": "",
+                  "contentType": "_plugin"
+              },
+              "settings": [
+                  {
+                      "key": "build_instance_url",
+                      "label": "Build Instance URL",
+                      "value":"",
+                  },
+                  {
+                      "key": "build_webhook_url",
+                      "label": "Build Webhook URL",
+                      "value":"",
+                  }
+              ],
+              "description": "Integration with the Netlify. After adding the plugin, you will be able to build your page on Netlify within Flotiq.",
+              "contentTypeDefinitions": "",
+              "order": "-1",
+              "disabled": false
+            }
+        },
+      );
+    },
+  );
